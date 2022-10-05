bb060d551e67fb5d2f5b4fe8ba2a56706e6ee00c
antoinde <antonello.indelicato98@gmail.com>
2022-10-05 18:07:51 +0200

    Aggiunti componenti: Header, Main, Footer

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div id="app">
-    
+    <!-- HEADER -->
+    <HeaderComponent />
+    <!-- MAIN -->
+    <MainComponent />
+    <!-- FOOTER -->
+    <FooterComponent />
   </div>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    
-  }
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  },
 }
 </script>
 
 <style lang="scss">
+@import '@/assets/style/style.scss';
+
 
 </style>
