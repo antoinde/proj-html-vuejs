a36a9848c985e9bfb32db7f0cea8667da86dd3c5
antoinde <antonello.indelicato98@gmail.com>
2022-10-07 11:46:47 +0200

    Sezione 1: Nav, completato

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,29 @@
 <template>
     <main>
-        
+        <nav >
+          <div class="container">
+            <a>
+              <span class="icon"> <i class="fa-solid fa-chart-column"></i> </span>
+              <span class="text"> Business </span>
+            </a>
+            <a>
+              <span class="icon"> <i class="fa-solid fa-palette"></i> </span>
+              <span class="text"> Design </span>
+            </a>
+            <a>
+              <span class="icon"> <i class="fa-solid fa-gear"></i> </span>
+              <span class="text"> Development </span>
+            </a>
+            <a>
+              <span class="icon"> <i class="fa-regular fa-face-smile-beam"></i> </span>
+              <span class="text"> Lifestyle </span>
+            </a>
+            <a>
+              <span class="icon"> <i class="fa-solid fa-print"></i> </span>
+              <span class="text"> Office Productivity </span>
+            </a>
+          </div>
+        </nav>
     </main>
 </template>
 
@@ -11,5 +34,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+nav {
+  background-color: var(--footer-nav-color);
+  margin-bottom: 20px;
+}
+.container{
+  display: flex;
+  column-gap: 45px;
+  justify-content: center;
+  padding: 15px;
+  .icon i{
+    color: #fff;
+    margin-right: 5px;
+  }
+  .text {
+    color: #fff;
+  }
+}
 </style>
