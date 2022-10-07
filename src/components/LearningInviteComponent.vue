57748b0d120d10eaf1a4da2309825a4112f0fe52
antoinde <antonello.indelicato98@gmail.com>
2022-10-07 22:01:57 +0200

    LearningInviteComponent completato

---
+++ src/components/LearningInviteComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <section>
+       <div class="text-area">
+        <h1>Limitless leaning,<br>more possibilities</h1>
+        <p>Online courses open the opportunity for learning to almost anyone, regardless of their scheduling commitmens.</p>
+        <a class="typeButtonRose" href="#">READ MORE</a>
+       </div>
+    </section>
+</template>
+
+<script>
+
+
+export default {
+    name: "LearningInviteComponent",
+}
+</script>
+
+<style lang="scss" scoped>
+section{
+    position: relative;
+    padding: 50px 0px;
+    height: 350px;
+    background-image: url('@/assets/img/bg.png');
+}
+.text-area{
+    position: absolute;
+    width: 500px;
+    left: 25%;
+    top: 70px;
+    h1{
+        font-size: 2.6rem;
+        margin-bottom: 10px;
+    }
+    p{
+        margin-bottom: 55px;
+    }
+}
+
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -4,16 +4,18 @@
       <PopularCoursesComponent
       :Cards="Cards"
       />
+      <LearningInviteComponent />
     </main>
 </template>
 
 <script>
 import AffiliateComponent from "./AffiliateComponent.vue";
 import PopularCoursesComponent from "./PopularCoursesComponent.vue";
+import LearningInviteComponent from "./LearningInviteComponent.vue";
 
 export default {
     name: "MainComponent",
-    components: { AffiliateComponent, PopularCoursesComponent },
+    components: { AffiliateComponent, PopularCoursesComponent, LearningInviteComponent },
     props: {
       Cards: Array
     }
